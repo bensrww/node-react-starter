4fe9e7344c23fac2eff81e08e53d7528a038ba1a
Ben Chong <bensrww@gmail.com>
2020-09-13 02:10:53 +0800

    use react-router to set props

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.jsx";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+import * as serviceWorker from './serviceWorker';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { TEAM_NUMBER } from './constants';
 
+const { TEAM_2, TEAM_3 } = TEAM_NUMBER;
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/t2">
+          <App teamNumber={TEAM_2} />
+        </Route>
+        <Route path="/t3">
+          <App teamNumber={TEAM_3} />
+        </Route>
+      </Switch>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
